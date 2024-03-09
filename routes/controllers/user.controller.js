1ad86ddd6c5f5cbaf2109446d7ff077c51a93b4c
Markost1 <markostevovic565@gmail.com>
2024-03-09 16:40:40 +0100

    kreiranje kontroler funkcije

---
+++ routes/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req,res) =>{
+    res.json({
+        "message":"json tekst poslat iz controllera"
+    })
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,15 @@
 import express from 'express'
+import { test } from './controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test',(req,res)=>{
-    res.status(200).json({
-        "message":"server odgovara sa json formatom kod je struktuiran modularan "
-    })
-})
+// router.get('/test',(req,res)=>{
+//     res.status(200).json({
+//         "message":"server odgovara sa json formatom kod je struktuiran modularan "
+//     })
+// })
+
+router.get('/test', test)
 
 
 export default router;
